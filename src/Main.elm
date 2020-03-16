e7a83552c07ec403926869a53b275fe296d5d318
atamyo <tamayo.angeline@gmail.com>
2020-03-15 21:36:20 -0400

    :rotating_light: Run elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,6 @@ module Main exposing (..)
 
 -- Keep track of edgic
 
-
 import Browser
 import Html exposing (Html, li, text, ul)
 import Html.Events exposing (onClick)
@@ -13,33 +12,35 @@ import Html.Events exposing (onClick)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 
 -- MODEL
 
 
-type alias Model = List String
+type alias Model =
+    List String
 
 
 init : Model
 init =
-  []
+    []
 
 
 
 -- UPDATE
 
 
-type Msg = NoOp
+type Msg
+    = NoOp
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp ->
-      model
+    case msg of
+        NoOp ->
+            model
 
 
 
@@ -48,7 +49,7 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  ul [] [
-    li [] <| List.intersperse (text " ") [text "Parvati", text "CPP4", text "MOR3"]
-    , li [] <| List.intersperse (text " ") [text "Michele", text "UTR2", text "UTR2"]
-  ]
+    ul []
+        [ li [] <| List.intersperse (text " ") [ text "Parvati", text "CPP4", text "MOR3" ]
+        , li [] <| List.intersperse (text " ") [ text "Michele", text "UTR2", text "UTR2" ]
+        ]

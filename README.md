b943c7f7091e2cc44376dc77bc5afe471c9b5986
atamyo <tamayo.angeline@gmail.com>
2020-03-15 20:56:35 -0400

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Edgic
+
+Track a reality TV contestant's [edgic](http://insidesurvivor.com/survivor-edgic-an-introduction-3094).
---
+++ src/Main.elm
@@ -0,0 +1,54 @@
+module Main exposing (..)
+
+-- Keep track of edgic
+
+
+import Browser
+import Html exposing (Html, li, text, ul)
+import Html.Events exposing (onClick)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model = List String
+
+
+init : Model
+init =
+  []
+
+
+
+-- UPDATE
+
+
+type Msg = NoOp
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    NoOp ->
+      model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  ul [] [
+    li [] <| List.intersperse (text " ") [text "Parvati", text "CPP4", text "MOR3"]
+    , li [] <| List.intersperse (text " ") [text "Michele", text "UTR2", text "UTR2"]
+  ]
